1dd6f5fe6ff9fa0aec2b3b5e197727d20f0a45d4
levina <82658782+levina-lab@users.noreply.github.com>
2021-07-18 04:12:57 +0700

    Update __main__.py

--- telegram_bot/__main__.py
+++ telegram_bot/__main__.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from anjani_bot import start
+from telegram_bot import start
 
 if __name__ == "__main__":
     start()
