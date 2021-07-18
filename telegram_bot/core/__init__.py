5c40b937582f3ae834bb2c1bc153855f82a69f1b
levina <82658782+levina-lab@users.noreply.github.com>
2021-07-19 00:53:03 +0700

    fixed

--- telegram_bot/core/__init__.py
+++ telegram_bot/core/__init__.py
@@ -13,5 +13,5 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from .anjani import Anjani
+from .telegramgroupbot import TelegramGroupBot
 from .logging import *
