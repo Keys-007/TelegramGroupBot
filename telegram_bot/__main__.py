4071dc778c7022734db32b08465ec76d609f5fe3
levina <82658782+levina-lab@users.noreply.github.com>
2021-07-19 16:44:45 +0700

    Update __main__.py

--- telegram_bot/__main__.py
+++ telegram_bot/__main__.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import logging
 from telegram_bot import start
 
 LOGGER = logging.getLogger(__name__)
