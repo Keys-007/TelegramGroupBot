e29b6511e38c861f5c4972a915bf48454c79dc76
levina <82658782+levina-lab@users.noreply.github.com>
2021-07-19 16:36:47 +0700

    Update __main__.py

--- telegram_bot/__main__.py
+++ telegram_bot/__main__.py
@@ -14,9 +14,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from telegram_bot import start, LOGS
+from telegram_bot import start, LOGGER
 
 if __name__ == "__main__":
     start()
-    LOGS.info(
+    LOGGER.info(
         f"BOT STARTED")
